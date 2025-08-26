d53aa411387d571297f9a6003c17cfb6e39f1989
Stefani <sgancheva15@gmail.com>
2025-08-26 18:06:24 +0300

    fix React + Vite set up

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   //const for images and counter for randomizer
   const [image, setImage] = useState(null);
   const [count, setCount] = useState(0);
+  const [time, setTime] = useState(null);
 
   const fetchImage = async () => {
 
@@ -13,10 +14,13 @@ function App() {
     const url_API = `https://picsum.photos/200?random=${count}`;
       setImage(url_API);
       setCount(prev=>prev+1); //increment the counter every time the button is clicked as well
+
+      const time_now = new Date().toLocaleString();
+      setTime(time_now);
     }
 
     catch(error){
-      console.log("Error fetching API", error);
+      console.log("Error fetching image", error);
 
     }
 
@@ -30,6 +34,7 @@ function App() {
     {image && (
       <div>
          <img src={image} alt="Random" />
+         <p>Last Image Fetched at: {time}</p>
       </div>
     )}
 
