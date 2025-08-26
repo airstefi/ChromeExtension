fabf4e3e20a501a3100bc54810c8a8b00dc07209
Stefani <sgancheva15@gmail.com>
2025-08-26 16:46:06 +0300

    Image randomizer with API

--- my-extension/src/App.jsx
+++ my-extension/src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const for images and counter for randomizer
+  const [image, setImage] = useState(null);
+  const [count, setCount] = useState(0);
+
+  const fetchImage = async () => {
+
+    try{
+      //API initialization + counter used for randomness 
+    const url_API = `https://picsum.photos/200?random=${count}`;
+      setImage(url_API);
+      setCount(prev=>prev+1); //increment the counter every time the button is clicked as well
+    }
+
+    catch(error){
+      console.log("Error fetching API", error);
+
+    }
+
+  }
 
   return (
     <>
+    <h1> Image Generator</h1>
+    <button onClick={fetchImage}>New Image</button>
+
+    {image && (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+         <img src={image} alt="Random" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    )}
+
     </>
   )
 }
